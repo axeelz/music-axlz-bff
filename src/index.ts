cf02639712c6b7998b24a1d3a9f2ed65509349a7
axeelz <axelzareb@hotmail.fr>
2025-07-17 00:16:15 +0200

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { Effect, Logger } from "effect";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { KVService } from "./services/kv";
 import { getPlaylist } from "./services/playlist";
 import { getRandomTrack } from "./services/random";
 import { formatTrackArtists, toErrorResponse } from "./utils/format";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
+
+app.use("*", cors());
 
 app.get("/", (c) => {
   const program = Effect.gen(function* () {
