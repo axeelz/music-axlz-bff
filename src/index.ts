8739b223f3169856f4960c4bc6cf8e26ac84a5a1
axeelz <axelzareb@hotmail.fr>
2025-07-21 01:10:45 +0200

    fix: CORS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import { Effect, Logger } from "effect";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { KVService } from "./services/kv";
 import { getPlaylist } from "./services/playlist";
 import { getShuffledTrack } from "./services/random";
 import { formatTrackArtists, toErrorResponse } from "./utils/format";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
+
+app.use(
+  "*",
+  cors({
+    origin: "https://axeelz.com",
+    credentials: true,
+  }),
+);
 
 app.get("/", (c) => {
   const program = Effect.gen(function* () {
